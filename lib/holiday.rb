e87fab9d8282a8a099d13bddbe18a3e98807d4a5
Adriana DiPietro <am20dipi@alum.siena.edu>
2020-09-17 17:58:13 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,8 +54,9 @@ def all_supplies_in_holidays(holiday_hash)
 end
 
 def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  holiday_hash.map do |season, holidays|
+    holidays.map do |holiday_name, supplies|
+      binding.pry
 
 end
 
